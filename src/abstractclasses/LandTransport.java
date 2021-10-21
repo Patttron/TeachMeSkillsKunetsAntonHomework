f3c8464affb4d40fa319734657dd562bbba1b572
Patttron <Realisttt19@gmail.com>
2021-10-21 22:53:02 +0300

    Added unit4

---
+++ src/abstractclasses/LandTransport.java
@@ -0,0 +1,12 @@
+package abstractclasses;
+
+abstract public class LandTransport extends Transport {
+    public int numberOfWheels;
+    public int fuelConsumption;
+
+    public LandTransport(int power, int maxSpeed, int weight, String transportBrand, int numberOfWheels, int fuelConsumption) {
+        super(power, maxSpeed, weight, transportBrand);
+        this.numberOfWheels = numberOfWheels;
+        this.fuelConsumption = fuelConsumption;
+    }
+}
---
+++ src/transportclasses/MilitaryAirTransport.java
@@ -0,0 +1,30 @@
+package transportclasses;
+
+import abstractclasses.AirTransport;
+
+public class MilitaryAirTransport extends AirTransport {
+    boolean bailoutSystem;
+    int missilesOnBoard;
+
+    public MilitaryAirTransport(int power, int maxSpeed, int weight, String transportBrand, int wingSpan, int minRunwayLength, boolean bailoutSystem, int missilesOnBoard) {
+        super(power, maxSpeed, weight, transportBrand, wingSpan, minRunwayLength);
+        this.bailoutSystem = bailoutSystem;
+        this.missilesOnBoard = missilesOnBoard;
+    }
+
+    public String checkBailoutSystem() {
+        return bailoutSystem ? "has bailout system" : "doesn't have bailout system!";
+    }
+
+    public String shoot() {
+        return (missilesOnBoard != 0) ? "Missile fired!" : "No ammunition";
+    }
+
+    @Override
+    public String toString() {
+        return super.transportBrand + " has power " + super.powerTransformation() + " kilowatts, max speed " + super.maxSpeed +
+                "km/h, weight " + super.weight + "kg, " + super.wingSpan + " metres wing span, \nneed " +
+                super.minRunwayLength + " metres minimum runway length, has " + missilesOnBoard +
+                " missiles on the board and " + checkBailoutSystem();
+    }
+}
